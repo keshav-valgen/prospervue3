dad0f19419077bbe1cc70068e9d3c94139521286
keshav-valgen <keshav@valgen.com>
2017-05-05 13:34:46 +0530

    a

--- R/segment.R
+++ R/segment.R
@@ -11,11 +11,11 @@ segment <- function(access_token, instance_url, object, field, newname){
 
 instance_u <- paste0(instance_url,'/')
 api <- '36.0'
-
-myquery <- paste0('Select Id, ', field,' FROM ', object)
+myobject <- object
+myquery <- paste0('Select Id, ', field,' FROM ', myobject)
 session <- c(sessionID = access_token,instanceURL = instance_u, apiVersion = api)
 
-data1 <- rforcecom.bulkQuery(session, myquery, object)
+data1 <- rforcecom.bulkQuery(session, myquery, myobject)
 data1 <- na.omit(data1)
 
 # Data Treatment starts Here
@@ -26,13 +26,14 @@ data1 <- subset(data1, select = c("Id", "dist"))
 colnames(data1) <- c("strId", "dist")
 
 
-# update_job <- rforcecom.createBulkJob(session,
-#                                       operation ='update', object = object) # Create a new bulkjob for updating Salesforce
-# my_data <- data.frame(id = data1$strId, v2 = data1$dist) # Dataframe to be uploaded
-# colnames(my_data) <- c("id", newname)
-#
-# batches_info <- rforcecom.createBulkBatch(session,
-#                                           jobId = update_job$id, data = my_data) #Update job
+update_job <- rforcecom.createBulkJob(session,
+                                      operation ='update', object = myobject) # Create a new bulkjob for updating Salesforce
+my_data <- data.frame(id = data1$strId, v2 = data1$dist) # Dataframe to be uploaded
+colnames(my_data) <- c("id", newname)
+
+batches_info <- rforcecom.createBulkBatch(session,
+                                          jobId = update_job$id, data = my_data) #Update job
+close_job_info <- rforcecom.closeBulkJob(session, jobId=update_job$id)
 
 return(nrow(data1))
 }
