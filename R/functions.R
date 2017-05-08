0b90629cd357c0746a82da44a54114c993b14d59
keshav-valgen <keshav@valgen.com>
2017-05-08 11:37:39 +0530

    1

--- R/functions.R
+++ R/functions.R
@@ -187,22 +187,22 @@ slider2 <- function(var, iter) # 2 inputs; The numeric variable to convert and N
   }
 }
 
-updater <- function(access_token, instance_url, myobject, data){
-
-  instance_u <- paste0(instance_url,'/')
-  api <- '36.0'
-  session <- c(sessionID = access_token,instanceURL = instance_u, apiVersion = api)
-
-  update_job <- rforcecom.createBulkJob(session,
-                                        operation ='update', object = myobject) # Create a new bulkjob for updating Salesforce
-  my_data <- data # Dataframe to be uploaded
-  batches_info <- rforcecom.createBulkBatch(session,
-                                            jobId = update_job$id, data = my_data) #Update job
-  close_job_info <- rforcecom.closeBulkJob(session, jobId=update_job$id)
-
-  rforcecom.logout(session)
-
-}
+# updater <- function(access_token, instance_url, myobject, data){
+#
+#   instance_u <- paste0(instance_url,'/')
+#   api <- '36.0'
+#   session <- c(sessionID = access_token,instanceURL = instance_u, apiVersion = api)
+#
+#   update_job <- rforcecom.createBulkJob(session,
+#                                         operation ='update', object = myobject) # Create a new bulkjob for updating Salesforce
+#   my_data <- data # Dataframe to be uploaded
+#   batches_info <- rforcecom.createBulkBatch(session,
+#                                             jobId = update_job$id, data = my_data) #Update job
+#   close_job_info <- rforcecom.closeBulkJob(session, jobId=update_job$id)
+#
+#   rforcecom.logout(session)
+#
+# }
 
 
 
