f121d86e8f9ef3d6954175adc85ee0d11d9a07f1
keshav-valgen <keshav@valgen.com>
2017-05-09 16:15:53 +0530

    5

--- R/quad.R
+++ R/quad.R
@@ -28,5 +28,5 @@ quad <- function(access_token, instance_url, object, field1, field2, newname){
   newdata <- subset(newdata, select = c('Id', 'Rank'))
   colnames(newdata) <- c("Id", newname)
   updater(access_token, instance_url, myobject, newdata)
-  return(newdata)
+  return(head(newdata))
 }
